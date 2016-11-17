0389c42cebaa7af27c5af6a165bdff0319c1df42
Kosuke Matsuda <matsuda@1020409.local>
2016-11-17 13:43:51 +0900

    create project

---
+++ DrawerTransition/DrawerTransition.h
@@ -0,0 +1,19 @@
+//
+//  DrawerTransition.h
+//  DrawerTransition
+//
+//  Created by Kosuke Matsuda on 2016/11/17.
+//  Copyright © 2016年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DrawerTransition.
+FOUNDATION_EXPORT double DrawerTransitionVersionNumber;
+
+//! Project version string for DrawerTransition.
+FOUNDATION_EXPORT const unsigned char DrawerTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawerTransition/PublicHeader.h>
+
+
